fd32f7187a31476b1dd9494e88d3af10a8cfd282
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-04-01 21:00:31 +0200

    adds catalog and data

---
+++ src/catalog.ts
@@ -0,0 +1,32 @@
+export const catalogData = {
+  booksByIsbn: {
+    '978-1779501127': {
+      isbn: '978-1779501127',
+      title: 'Watchmen',
+      publicationYear: 1987,
+      authorIds: ['alan-moore', 'dave-gibbons'],
+      bookItems: [
+        {
+          id: 'book-item-1',
+          libId: 'nyc-central-lib',
+          isLent: true,
+        },
+        {
+          id: 'book-item-2',
+          libId: 'nyc-central-lib',
+          isLent: false,
+        },
+      ],
+    },
+  },
+  authorsById: {
+    'alan-moore': {
+      name: 'Alan Moore',
+      bookIsbns: ['978-1779501127'],
+    },
+    'dave-gibbons': {
+      name: 'Dave Gibbons',
+      bookIsbns: ['978-1779501127'],
+    },
+  },
+};
